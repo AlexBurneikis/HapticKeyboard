92c69eca57b132cd00b0a0ce8443dfe085a78786
Alex Burneikis <alexburneikis@gmail.com>
2022-09-30 09:36:18 +1000

    Initial Commit

---
+++ HapticKeyboard/HapticKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticKeyboardApp.swift
+//  HapticKeyboard
+//
+//  Created by Alexander Burneikis on 30/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct HapticKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
